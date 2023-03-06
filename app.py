9009d9440bab9026568de4fda71624d7b49046ef
GabrielAuger1 <gabrielauger@hotmail.com>
2023-03-06 08:58:15 -0500

    Update header

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
+import flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
-
 @app.route('/')
 def main_app():  # put application's code here
     return render_template('game.html')
